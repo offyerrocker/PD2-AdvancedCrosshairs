4ba22056248cd3cb7e5e4fc2c476d5d7b2feabac
offyerrocker <offyerrocker88@gmail.com>
2021-03-19 07:55:50 -0700

    Fixed vanilla hitmarkers lingering when option is disabled

--- lua/hudhitconfirm.lua
+++ lua/hudhitconfirm.lua
@@ -1,20 +1,20 @@
 HUDHitConfirm._orig_on_hit_confirmed = HUDHitConfirm.on_hit_confirmed
 function HUDHitConfirm:on_hit_confirmed(...)
 	if not AdvancedCrosshair:IsHitmarkerEnabled() then 
-		return self:_orig_on_hit_confirmed(self,...)
+		return self:_orig_on_hit_confirmed(...)
 	end
 end
 
 HUDHitConfirm._orig_on_hs_confirmed = HUDHitConfirm.on_headshot_confirmed
 function HUDHitConfirm:on_headshot_confirmed(...)
 	if not AdvancedCrosshair:IsHitmarkerEnabled() then 
-		return self:_orig_on_hs_confirmed(self,...)
+		return self:_orig_on_hs_confirmed(...)
 	end
 end
 
 HUDHitConfirm._orig_on_crit_confirmed = HUDHitConfirm.on_crit_confirmed
 function HUDHitConfirm:on_crit_confirmed(...)
 	if not AdvancedCrosshair:IsHitmarkerEnabled() then 
-		return self:_orig_on_crit_confirmed(self,...)
+		return self:_orig_on_crit_confirmed(...)
 	end
 end
